6b2bb45509a34f4a797eb646a754d96301e555e1
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-11-27 17:12:05 +0200

    add hover effects and flex column

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ for(let i = 0; i < 16; i++){
   for(let j = 0; j < 16; j++){
     const divBox = document.createElement("div");
     divBox.style.backgroundColor = "red";
-    divBox.style.minWidth = "40px";
-    divBox.style.height = "40px";
-    divBox.style.border = "1px solid";
+    
+    divBox.style.minWidth = `6`;
+    divBox.style.minHeight = "6%";
+    divBox.style.outline = "1px solid";
     divBox.setAttribute("class", "box-active")
    
     mainContainer.appendChild(divBox);
   }
   
 }
+
+
+//hover effect 
+const hoverEffect = document.querySelectorAll(".box-active");
+
+for (let elem of hoverEffect){
+  elem.addEventListener("mouseenter",function(){
+    elem.style.backgroundColor = "green";
+  })
+}
+
