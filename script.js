338f245fa2804b670e1e22b1c686ed7cd70bc5ac
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-11-27 17:44:13 +0200

    update the box creation in a function

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const mainContainer = document.querySelector(".container");
 
 
-//create a for loop that create div boxes 
+function drawBoxes(sizeOfBoxes){
+  // Because will always take square values like 16x16,32x32 for column and rows
+  // Only require 1 parameter
 
-for(let i = 0; i < 16; i++){
-  for(let j = 0; j < 16; j++){
-    const divBox = document.createElement("div");
-    divBox.style.backgroundColor = "red";
-    
-    divBox.style.minWidth = `6`;
-    divBox.style.minHeight = "6%";
-    divBox.style.outline = "1px solid";
-    divBox.setAttribute("class", "box-active")
-   
-    mainContainer.appendChild(divBox);
+  // to find the individual dimensions for each box in %
+  let boxDimensions = 100/sizeOfBoxes;
+
+  for(let i = 0; i < sizeOfBoxes; i++){
+    for(let j = 0; j < sizeOfBoxes; j++){
+      const divBox = document.createElement("div");
+      divBox.style.backgroundColor = "red";
+
+      divBox.style.minWidth = `${boxDimensions}%`;
+      divBox.style.minHeight = `${boxDimensions}%`;
+      divBox.style.outline = "1px solid";
+      divBox.setAttribute("class", "box-active")
+
+      mainContainer.appendChild(divBox);
+
+    }
   }
-  
 }
 
+drawBoxes(16);
 
 //hover effect 
 const hoverEffect = document.querySelectorAll(".box-active");
